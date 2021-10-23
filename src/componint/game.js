e750632d450de4c0f92bdef31dd49fafea71c696
RawnakAra <rawnak96@gmail.com>
2021-10-23 23:44:21 +0300

    section 2

--- src/componint/game.js
+++ src/componint/game.js
@@ -2,36 +2,48 @@ import React, { useState } from "react";
 import Board from "./Board";
 import calculateWinner from "./test";
 const Game = () => {
-    const [squares, setSquares] = useState(Array(9).fill(''))
+    const [squares, setSquares] = useState([Array(9).fill('')]);
     console.log(squares)
-    const [player ,setPlayer] = useState(true)
-    const winner = calculateWinner(squares);
-   
-   
-const squaresCopy = [...squares]
-const handleClick = (index)=>{
-    if(squaresCopy[index] || winner) return;
-    squaresCopy[index] = player ? '❎' :'🟢'
-    setSquares(squaresCopy)
-    setPlayer(!player)
-    const history = [] ;
-    history.push(squares)
-    console.log(squares)
-}
-const backOnTime =(index)=>{
-    const history = [] ;
-    history.push({squaresCopy})
-    console.log(history) 
-}
+    const [stepNumber, setStepNumber] = useState(0)
+    const [player, setPlayer] = useState(true);
+    const winner = calculateWinner(squares[stepNumber]);
+    //const [history , setHistory] = useState([])
+
+
+
+
+    const handleClick = (index) => {
+        const timeInHistory = squares.slice(0, stepNumber + 1)
+        const current = timeInHistory[stepNumber];
+        const squaresCopy = [...current]
+        if (squaresCopy[index] || winner) return;
+        squaresCopy[index] = player ? '❎' : '🟢'
+        setSquares([...timeInHistory, squaresCopy])
+        setStepNumber(timeInHistory.length)
+        setPlayer(!player)
+    }
+    const move = (i) => {
+        setPlayer(!player)
+        setStepNumber(i)
+    }
+
     return (
         <div className='game'>
             <h1>Tic Tac Toe</h1>
-            <Board SQ={squares} onClick={handleClick}/>
+            <Board SQ={squares[stepNumber]} onClick={handleClick} />
             <div className='secondContainer'>
-            
-            <div className='restart'><button onClick={()=>{setSquares(Array(9).fill('')) ; setPlayer(true)}}>Restart Game</button></div>
-            <div className='HowIsPlaying'>{winner ? 'The WinnerIs'+ winner: 'Playing:' + (player ? '❎' :'🟢')}</div>
-            <div className='steps'><button onClick={backOnTime}>Back On Time</button></div>
+                <div className='restart'><button onClick={() => { setSquares([Array(9).fill('')]); setPlayer(true); setStepNumber(0) }}>Restart Game</button></div>
+                <div className='HowIsPlaying'>{winner ? 'The WinnerIs' + winner : 'Playing:' + (player ? '❎' : '🟢')}</div>
+                <div className='steps' >
+                    {
+                    squares.map((e, i) => {
+                        return (
+                            <li key={e}>
+                                <button onClick={() => { move(i) }}>{i+1}</button>
+                            </li>
+                        )
+                    })}
+                    </div>
             </div>
         </div>
     )
